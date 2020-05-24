25385da6760f1a75c90748579f888bdb5090044d
RenZhanFeng <947625259@qq.com>
2020-05-25 00:18:32 +0800

    制作首页博客文章展示列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,26 @@ const app = getApp()
 
 Page({
   data: {
-   
-  }
+    blogList: null
+  },
+
   //事件处理函数
+  onLoad: function () {
+    let _this = this
+    wx.request({
+      url: "http://rogerblog.ren:8443/api/articles",
+      success: function (res) {
+        if (res.data.code === 200) { 
+          console.log(res.data.data.content)
+          _this.setData({
+            blogList: res.data.data.content
+          })
+        }
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  }
+
 })
