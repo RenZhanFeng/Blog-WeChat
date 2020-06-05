3cc3841a289003e70bf4b74bad57b4be1918b379
RenZhanFeng <947625259@qq.com>
2020-06-05 11:27:43 +0800

    '完善分类部分，实现分类超出屏幕的时候可以左右滑动。更换底部tabbar的图标。完成1.0.0版本，提交小程序上线审核'

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,12 +8,22 @@ Page({
     categories: null,
     currentTab: 0,
     isLoading: true, // 判断是否尚在加载中
+    navScrollLeft: 0
   },
 
   //加载小程序时获取博客列表和分类数据
   onLoad: function () {
     this.getBolgLIst()
     this.getCategories()
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          pixelRatio: res.pixelRatio,
+          windowHeight: res.windowHeight,
+          windowWidth: res.windowWidth
+        })
+      },
+    })
   },
   //点击博客列表跳转到博客详情
   goDetails(e) {
@@ -29,11 +39,18 @@ Page({
   //点击导航栏切换页面
   swichNav: function (e) {
     let _this = this
-    if (_this.data.currentTab === e.target.dataset.current) {
+    let cur = e.target.dataset.current;
+    let singleNavWidth = this.data.windowWidth / 5;
+    //控制导航栏的滚动条
+    this.setData({
+      navScrollLeft: (cur - 2) * singleNavWidth
+    })
+    //如果点击的是当前的tab，返回false,否则就是更新tab和数据
+    if (_this.data.currentTab === cur) {
       return false
     } else {
       _this.setData({
-        currentTab: e.target.dataset.current
+        currentTab: cur
       })
       _this.getBolgLIst(e.target.dataset.id)
     }
@@ -41,15 +58,18 @@ Page({
   //切换页面时更新currentTab
   swiperChange: function (e) {
     let _this = this
+    let cur = e.detail.current;
+    let singleNavWidth = this.data.windowWidth / 5;
     this.setData({
-      currentTab: e.detail.current
+      currentTab: cur,
+      navScrollLeft: (cur - 2) * singleNavWidth
     })
     _this.getBolgLIst(_this.data.categories[e.detail.current].id)
   },
   getBolgLIst(cid) {
     this.data.isLoading = true
     let _this = this
-    let api = !cid || cid == 16 ? `${app.globalData.APIURL}articles`:`${app.globalData.APIURL}articles/cid=${cid}`
+    let api = !cid || cid == 16 ? `${app.globalData.APIURL}articles` : `${app.globalData.APIURL}articles/cid=${cid}`
     wx.request({
       url: api,
       success: function (res) {
